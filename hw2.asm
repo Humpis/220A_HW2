c2dae238f22dc53c48ea788215e940411d3e9956
Vidar Minkovsky <vmsailor@gmail.com>
2016-09-26 15:38:41 -0400

    atoui

--- hw2.asm
+++ hw2.asm
@@ -10,11 +10,23 @@
 ##############################
 
 atoui:
-    #Define your code here
-	############################################
-	# DELETE THIS CODE. Only here to allow main program to run without fully implementing the function
-	li $v0, 0
-	############################################
+	li $v0, 0			# sum
+	
+atoui_loop:
+	lb $t1, ($a0)			# char at position in t0
+	beqz $t1, atoui_done		# end of string
+	blt $t1, '0', atoui_done	# less than 0
+	bgt $t1, '9', atoui_done	# greater than 9
+	li $t3, 10			# for mult
+	mult $v0, $t3
+	mflo $v0			# sum = sum*10
+	li $t4, '0'		
+	sub $t3, $t1, $t4		# char - 0
+	add $v0, $v0, $t3		# sum = sum + ^^
+	addi $a0, $a0, 1		# increent string
+	j atoui_loop
+	
+atoui_done:			
 	jr $ra
 
 uitoa:
